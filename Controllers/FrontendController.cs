35a3832c8855155fa50296336629bf13bc742a97
Samit Koyom <samitkoyom@gmail.com>
2022-06-02 15:52:43 +0700

    first commit

---
+++ Controllers/FrontendController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreInventory.Controllers
+{
+    public class FrontendController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Webdev()
+        {
+            return View();
+        }
+
+        public ActionResult Mobiledev()
+        {
+            return View();
+        }
+
+        public ActionResult Graphic()
+        {
+            return View();
+        }
+
+        // เรียกแสดงแบบฟอร์มลงทะเบียน
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // เรียกแสดงแบบฟอร์มลงเข้าสุ่ระบบ
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
